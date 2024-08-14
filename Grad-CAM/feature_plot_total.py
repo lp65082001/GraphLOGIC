5d6e5c2d4aa1f110a9a1093c59174dd15db970bd
AmbroseHui <40039920+lp65082001@users.noreply.github.com>
2024-08-14 22:40:00 +0800

    20240814

--- Grad-CAM/feature_plot_total.py
+++ Grad-CAM/feature_plot_total.py
@@ -13,10 +13,10 @@ def smooth(x):
     return np.linspace(0, 17,100),Y_
 
 
-save_dir = '../bert4_shuffle/'
+save_dir = '../bert4/'
 
 amino_table = {"Ala":0,"Cys":1,"Asp":2,"Glu":3,"Arg":4,"Ser":5,"Val":6}
-real_posl = np.array(["G12","P11","P10","G9","P8","P7","G6","P5","P4","G3","P2","P1","G0(mut)","P1'","P2'","G3'","P4'","P5'","G6'","P7'","P8'","G9'","P10'","P11'","G12'","P13'","P14'"])
+real_posl = np.array(["G12","P11","P10","G9","P8","P7","G6","P5","P4","G3","P2","P1","*G0","P1'","P2'","G3'","P4'","P5'","G6'","P7'","P8'","G9'","P10'","P11'","G12'","P13'","P14'"])
 
 a1_feature = np.load('Grad_CAM_total.npy')
 
@@ -32,28 +32,32 @@ a1 = 276
 a2 = 268
 '''
 # plot total Grad-CAM (a1) # 
-plt.plot((a1_feature[0,:,0]+a1_feature[0,:,1]+a1_feature[1,:,0]+a1_feature[1,:,1])/268, lw=1, color='lightcoral', linestyle='solid',label="$ɑ_1$")
+plt.plot((a1_feature[0,:,0]+a1_feature[0,:,1]+a1_feature[1,:,0]+a1_feature[1,:,1])/283, lw=1, color='lightcoral', linestyle='solid',label="$ɑ_1$")
 #plt.plot(a1_feature[0,:,1], lw=1, color='cornflowerblue', linestyle='solid',label="Lethal ($ɑ_1$)")
-plt.plot((a1_feature[0,:,2]+a1_feature[1,:,2])/276, lw=1, color='seagreen', linestyle='solid',label="$ɑ_2$")
+plt.plot((a1_feature[0,:,2]+a1_feature[1,:,2])/283, lw=1, color='seagreen', linestyle='solid',label="$ɑ_2$")
 
 plt.plot(12,0,"r*",label="mutation position")
 plt.legend(edgecolor="black",fontsize=12)
-plt.xticks(range(27),real_posl ,fontsize=12,rotation=90)
-plt.ylabel("normalized quantity (276)",fontsize=12)
+plt.xticks(range(27),real_posl ,fontsize=10,rotation=90)
+plt.ylabel("normalized quantity (283)",fontsize=12)
+plt.xlabel("amino acid position",fontsize=12)
 plt.ylim(0,1)
+plt.yticks(fontsize=12)
 #plt.ylim(0,0.5)
 plt.tight_layout()
 plt.savefig(f"../figure/Grad_CAM_total_a1.png",dpi=300,bbox_inches="tight")
 plt.close()
 
 # plot total Grad-CAM (a2) # 
-plt.plot((a1_feature[2,:,0]+a1_feature[2,:,1]+a1_feature[3,:,0]+a1_feature[3,:,1])/268, lw=1, color='lightcoral', linestyle='solid',label="$ɑ_1$")
+plt.plot((a1_feature[2,:,0]+a1_feature[2,:,1]+a1_feature[3,:,0]+a1_feature[3,:,1])/269, lw=1, color='lightcoral', linestyle='solid',label="$ɑ_1$")
 #plt.plot(a1_feature[0,:,1], lw=1, color='cornflowerblue', linestyle='solid',label="Lethal ($ɑ_1$)")
-plt.plot((a1_feature[2,:,2]+a1_feature[3,:,2])/268, lw=1, color='seagreen', linestyle='solid',label="$ɑ_2$")
+plt.plot((a1_feature[2,:,2]+a1_feature[3,:,2])/269, lw=1, color='seagreen', linestyle='solid',label="$ɑ_2$")
 plt.plot(12,0,"r*",label="mutation position")
 plt.legend(edgecolor="black",fontsize=12)
-plt.xticks(range(27),real_posl ,fontsize=12,rotation=90)
-plt.ylabel("normalized quantity (268)",fontsize=12)
+plt.xticks(range(27),real_posl ,fontsize=10,rotation=90)
+plt.ylabel("normalized quantity (269)",fontsize=12)
+plt.xlabel("amino acid position",fontsize=12)
+plt.yticks(fontsize=12)
 plt.ylim(0,1)
 #plt.ylim(0,0.5)
 plt.tight_layout()
